bb4c97923a4ca3dad9020473a9ee23db1ac94d94
ranjit4snowflake <127399541+ranjit4snowflake@users.noreply.github.com>
2023-03-09 17:39:57 +0530

    Update streamlit_app.py
    Omega

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ streamlit.title('My Parents New Healthy Diner');
 
 streamlit.header('Breakfast Menu');
 
-streamlit.subheader('Omega 3 & Blueberry Oatmeal');
+streamlit.caption('Omega 3 & Blueberry Oatmeal');
