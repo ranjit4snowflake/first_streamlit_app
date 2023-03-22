3235eb8bf31791591cabdfdd57812a977846963d
ranjit4snowflake <127399541+ranjit4snowflake@users.noreply.github.com>
2023-03-23 03:45:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected];
 #display the table on the page
 streamlit.dataframe(fruits_to_show);
 
+def get_fruitvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
 streamlit.header("Fruityvice Fruit Advice!")
 try:
  fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -42,12 +47,12 @@ try:
  else:
   #streamlit.write('The user entered ', fruit_choice)
   #import requests
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+  #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
   #streamlit.text(fruityvice_response.json())
   # write your own comment -what does the next line do? 
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   # write your own comment - what does this do?
-  streamlit.dataframe(fruityvice_normalized)
+  streamlit.dataframe(get_fruitvice_data(fruit_choice))
 
 except URLLError as e:
  streamlit.error()
