ddbe4cd3235c5c732daeee0a305075580502a711
ranjit4snowflake <127399541+ranjit4snowflake@users.noreply.github.com>
2023-03-23 04:45:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,7 +70,7 @@ except URLLError as e:
 #my_data_rows = my_cur.fetchall()
 #streamlit.text("Hello from Snowflake:")
 
-streamlit.header("View Our Fruit List - Add Your Favorites : ")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 #snowflake related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
